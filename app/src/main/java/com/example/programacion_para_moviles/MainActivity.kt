40a38042039be202bb6ea78a343fe26e2250d4bc
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-13 12:29:40 -0600

    feat: Add registration form in MainActivity and storage dependency

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             Programacion_Para_MovilesTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Formulario de Registro",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +35,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column{
+        Text(text = "Formulario de Registro")
+
+        Text(text = "Nombre:")
+        OutlinedTextField(value = "", onValueChange = {})
+        Text(text = "Email:")
+        OutlinedTextField(value = "", onValueChange = {})
+        Text(text = "Contraseña:")
+        OutlinedTextField(value = "", onValueChange = {})
+        Button(onClick = {/*TODO*/}) {
+            Text(text = "Registrarse")
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -45,3 +57,4 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
