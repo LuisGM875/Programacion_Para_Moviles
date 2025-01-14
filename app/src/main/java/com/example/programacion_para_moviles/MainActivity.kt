cc909daee2b987fee8c7c017a381c972fbf632e9
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-14 17:38:12 -0600

    fix: update design with padding

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -9,15 +9,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,43 +42,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
-            text = "Encuesta de Satisfacción",
+            text = "Encuesta de Satisfacción", modifier = modifier.padding(bottom = 16.dp)
         )
-
         Text(text = "Nombre:")
         OutlinedTextField(
             value = "",
             onValueChange = {},
         )
-
         Text(text = "Nivel de satisfacción:")
         RangeSlider(
-            value = 0f..0f,
+            value = 1f..1f,
             onValueChange = {},
-            valueRange = 1f..5f,
+            valueRange = 1f..10f,
         )
-
         Text(text = "Comentarios:")
         OutlinedTextField(
             value = "",
             onValueChange = {},
             maxLines = 4,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(120.dp)
         )
-
         Button(
             onClick = { /* TODO */ },
         ) {
             Text(text = "Enviar")
         }
     }
-
 }
 
 @Preview(showBackground = true)
