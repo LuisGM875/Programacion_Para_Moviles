8d8008493ebff636c009715b4456317196122610
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-14 17:08:43 -0600

    fix: update interface to Formulario de Registro

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,8 +13,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.programacion_para_moviles.ui.theme.Programacion_Para_MovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,16 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column{
-        Text(text = "Formulario de Registro")
-
-        Text(text = "Nombre:")
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "Formulario de Registro", modifier = Modifier.padding(16.dp))
+        Text(text = "Nombre:", modifier = Modifier.padding(4.dp))
         OutlinedTextField(value = "", onValueChange = {})
-        Text(text = "Email:")
+        Text(text = "Email:", modifier = Modifier.padding(4.dp))
         OutlinedTextField(value = "", onValueChange = {})
-        Text(text = "Contraseña:")
+        Text(text = "Contraseña:",  modifier = Modifier.padding(4.dp))
         OutlinedTextField(value = "", onValueChange = {})
-        Button(onClick = {/*TODO*/}) {
+        Button(onClick = {/*TODO*/}, modifier = Modifier.padding(16.dp)) {
             Text(text = "Registrarse")
         }
     }
