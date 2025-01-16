bfab37d531494984ac0cdf491752973a273eeb75
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-15 21:49:12 -0600

    feat: Add feedback form in MainActivity

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.programacion_para_moviles.ui.theme.Programacion_Para_MovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +42,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Deja un Comentario")
+        Text(text = "Nombre:")
+        Text(text= " ")
+        OutlinedTextField(
+            value = "",
+            onValueChange = {  },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Text(text = "Email:")
+        OutlinedTextField(
+            value = "",
+            onValueChange = {  },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Text(text = "Comentario:")
+        OutlinedTextField(
+            value = "",
+            onValueChange = {  },
+            label = { Text("Comentario") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp),
+            maxLines = 4
+        )
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .padding(top = 16.dp)
+        ) {
+            Text(text = "Enviar")
+        }
+    }
 }
 
 @Preview(showBackground = true)
