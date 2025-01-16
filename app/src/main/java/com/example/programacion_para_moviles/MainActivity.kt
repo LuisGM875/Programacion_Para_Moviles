3971c282616caf9cdfc79f34836265f0efb7d7d4
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-16 15:37:55 -0600

    feat: Add Contact Form in MainActivity

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.programacion_para_moviles.ui.theme.Programacion_Para_MovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +42,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Formulario de Contacto")
+        Text(text= "Nombre", modifier = Modifier.padding(8.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {  },
+        )
+        Text(text= "Teléfono", modifier = Modifier.padding(8.dp))
+        OutlinedTextField(
+            value = " ",
+            onValueChange = { },
+            label = {  },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+        )
+        Text(text= "Mensaje", modifier = Modifier.padding(8.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = {  },
+            modifier = Modifier.padding(8.dp)
+                .size(width = 300.dp, height = 100.dp),
+            maxLines = 4
+        )
+        Button(
+            onClick = { },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Enviar")
+        }
+    }
 }
 
 @Preview(showBackground = true)
