7ae05a02b61b098d2c6b73d53e34905fe1b34944
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-16 17:40:29 -0600

    feat: Add registration form to an event in MainActivity

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.programacion_para_moviles.ui.theme.Programacion_Para_MovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +33,7 @@ class MainActivity : ComponentActivity() {
             Programacion_Para_MovilesTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Inscripción a un Evento",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +44,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var selectedOption by remember { mutableStateOf<String?>(null) }
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Inscripción a un Evento", modifier = Modifier.padding(start = 8.dp))
+        Text(text = "Ingresa tu nombre:", modifier = Modifier.padding(start = 4.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+        )
+        Text(text = "Email:", modifier = Modifier.padding(start = 4.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+        )
+        Text(text = "¿Asistirá al evento?")
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = selectedOption == "si",
+                onClick = { selectedOption = "si" }
+            )
+            Text(text = "Sí", modifier = Modifier.padding(start = 4.dp))
+
+            RadioButton(
+                selected = selectedOption == "no",
+                onClick = { selectedOption = "no" }
+            )
+            Text(text = "No", modifier = Modifier.padding(start = 4.dp))
+        }
+
+        Button( onClick = { }) {
+            Text(text = "Inscribirse")
+        }
+    }
 }
 
 @Preview(showBackground = true)
