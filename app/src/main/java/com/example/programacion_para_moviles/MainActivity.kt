c9db3acfce6eff6081bc5fe90cb2b6c3f1ca2785
LuisGM875 <112772449+LuisGM875@users.noreply.github.com>
2025-01-16 14:58:32 -0600

    feat: Update the main screen to include a preference survey with text fields and a submit button.

--- app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
+++ app/src/main/java/com/example/programacion_para_moviles/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.programacion_para_moviles.ui.theme.Programacion_Para_MovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +28,7 @@ class MainActivity : ComponentActivity() {
             Programacion_Para_MovilesTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Encuesta de Preferencias",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,11 +38,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( name: String, modifier: Modifier = Modifier ) {
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Encuesta de Preferencias", fontSize = 24.sp)
+        Text(text = "Color Favorito:", modifier = Modifier.padding(20.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {   },
+        )
+        Text("Comida Favorita:", modifier = Modifier.padding(20.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+        )
+        Button(
+            onClick = {  },
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Text("Enviar")
+        }
+    }
 }
 
 @Preview(showBackground = true)
